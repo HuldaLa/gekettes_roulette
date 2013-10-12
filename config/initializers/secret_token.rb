e36ed4471ee2f6f9f29637510172438b626e1fad
Anne Lamprecht <anne.lamprecht@hanseventures.com>
2013-10-12 12:15:21 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeekettesRoulette::Application.config.secret_token = 'a7195a8fec52466107c9945e1cdaca4f6f9f5ccbeac09acf586ec22bce07b9e050749c66347c4529f89c7a5b8b41f6fff9e528e24a5e032cc770de5f69f932c7'
